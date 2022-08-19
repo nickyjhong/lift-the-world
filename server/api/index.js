05c81b28a2c2de174dcb5832fd00bf0debc2f4c1
Cherry Xu <ms.cherry.xu@gmail.com>
2022-08-19 13:56:01 -0400

    Fixed Router error by adding exercise route in index

--- server/api/index.js
+++ server/api/index.js
@@ -1,12 +1,11 @@
-const router = require('express').Router()
-module.exports = router
+const router = require('express').Router();
+module.exports = router;
 
-router.use('/users', require('./users'))
+router.use('/users', require('./users'));
+router.use('/exercise', require('./exercise'));
 
 router.use((req, res, next) => {
-  const error = new Error('Not Found')
-  error.status = 404
-  next(error)
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
 });
-
-router.use('/exercise', require('./exercise'));
