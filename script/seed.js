17998970c957ecc2b61bd607282e8e8141e546e2
nickyjhong <nicolehong95@gmail.com>
2022-08-19 15:34:40 -0400

    added some preset workout data

--- script/seed.js
+++ script/seed.js
@@ -1,45 +1,73 @@
-const {
-  db,
-  models: { User, Exercise, Workout, WorkoutList },
-} = require('../server/db');
+const {db, models: { User, Exercise, Workout, WorkoutList }} = require('../server/db');
 
-const dummyExercises = [
+const upperExercises = [
   {
-    name: 'bench press',
+    name: 'Bench press',
     category: 'chest',
     equipment: 'barbell, weights, bench',
     tipsAndTricks: 'keep shoulders back, don\'t bounce off your chest, use a spotter if needed',
     youtubeLink: 'https://www.youtube.com/watch?v=gRVjAtPip0Y'
-  },
-  {
-    name: 'lat pulldown',
+  }, {
+    name: 'Lat pulldown',
     category: 'back',
     equipment: 'pulldown machine',
     tipsAndTricks: 'use slow and controlled movements, keep shoulders back, keep back straight',
     youtubeLink: 'https://www.youtube.com/watch?v=Z_3xHwuO8Tk'
-  },
-  {
-    name: 'back squat',
-    category: 'legs',
-    equipment: 'barbell, weights, squat wrack',
-    tipsAndTricks: 'keep your back straight, push through your heels, don\'t lock your knees',
-    youtubeLink: 'https://www.youtube.com/watch?v=Dy28eq2PjcM'
-  },
-  {
-    name: 'preacher curl',
+  }, {
+    name: 'Preacher curl',
     category: 'arms',
     equipment: 'dumbell or barbell, curling bench',
     tipsAndTricks: 'utilize full range of motion, move slow and controlled movements',
     youtubeLink: 'https://www.youtube.com/watch?v=fIWP-FRFNU0'
-  },
+  }, {
+    name: 'Shoulder press',
+    category: 'back',
+    equipment: 'dumbbell or barbell',
+    tipsAndTricks: '',
+    youtubeLink: 'https://www.youtube.com/watch?v=5yWaNOvgFCM',
+  }, {
+    name: 'Row',
+    category: 'back',
+    equipment: 'dumbbell or barbell',
+    tipsAndTricks: '',
+    youtubeLink: 'https://www.youtube.com/watch?v=roCP6wCXPqo'
+  }
+]
+
+const lowerExercises = [
   {
-    name: 'leg raises',
+    name: 'Squat',
+    category: 'legs',
+    equipment: 'barbell, weights, squat rack',
+    tipsAndTricks: 'keep your back straight, push through your heels, don\'t lock your knees',
+    youtubeLink: 'https://www.youtube.com/watch?v=Dy28eq2PjcM'
+  }, {
+    name: 'Leg raises',
     category: 'core',
     equipment: 'floor mat',
     tipsAndTricks: 'engage your core, use controlled movements',
     youtubeLink: 'https://www.youtube.com/watch?v=JB2oyawG9KI'
-  },
-];
+  }, {
+    name: 'Romanian deadlift',
+    category: 'legs',
+    equipment: 'dumbbell or barbell',
+    tipsAndTricks: '',
+    youtubeLink: 'https://www.youtube.com/watch?v=7AaaYhMqTws',
+  }, {
+    name: 'Leg curl',
+    category: 'legs',
+    equipment: 'leg curl machine',
+    tipsAndTricks: '',
+    youtubeLink: 'https://www.youtube.com/watch?v=fK0uZ3KRZRI',
+  }, {
+    name: 'Leg extension',
+    category: 'legs',
+    equipment: 'leg extension machine',
+    tipsAndTricks: '',
+    youtubeLink: 'https://www.youtube.com/watch?v=8Jqof7z3QYM',
+  }
+]
+
 
 const dummySets = [
   {
@@ -60,11 +88,8 @@ async function seed() {
   await db.sync({ force: true });
   console.log('db synced!');
 
-  await Promise.all(
-    dummyExercises.map((exercise) => {
-      return Exercise.create(exercise);
-    })
-  );
+  await Promise.all(upperExercises.map((exercise) => Exercise.create(exercise)));
+  await Promise.all(lowerExercises.map((exercise) => Exercise.create(exercise)));
 
   // Creating Users
   const kyle = await User.create({
@@ -111,16 +136,32 @@ async function seed() {
     level: 24
   });
 
-  const exercise1 = await Exercise.findByPk(1);
-  const exercise2 = await Exercise.findByPk(2);
-  const exercise3 = await Exercise.findByPk(3);
-  const exercise4 = await Exercise.findByPk(4);
-  const exercise5 = await Exercise.findByPk(5);
+  const upper1 = await Exercise.findByPk(1);
+  const upper2 = await Exercise.findByPk(2);
+  const upper3 = await Exercise.findByPk(3);
+  const upper4 = await Exercise.findByPk(4);
+  const upper5 = await Exercise.findByPk(5);
+  const lower1 = await Exercise.findByPk(6);
+  const lower2 = await Exercise.findByPk(7);
+  const lower3 = await Exercise.findByPk(8);
+  const lower4 = await Exercise.findByPk(9);
+  const lower5 = await Exercise.findByPk(10);
 
   const workout1 = await Workout.create({ status: 'closed' });
+  const psworkout1 = await Workout.create({ status: 'active' });
+  const psworkout2 = await Workout.create({ status: 'active' });
+  const psworkout3 = await Workout.create({ status: 'active' });
+  const psworkout4 = await Workout.create({ status: 'active' });
+
+  await psworkout1.addExercises([upper1, upper2, upper3])
+  await psworkout2.addExercises([upper3, upper4, upper5])
+  await psworkout3.addExercises([lower1, lower2, lower3])
+  await psworkout4.addExercises([lower3, lower4, lower5])
+
+  // ADD IN REPS HERE
 
   await workout1.setUser(kyle);
-  await workout1.addExercise(exercise1);
+  await workout1.addExercise(upper1);
 
   const closed1 = await WorkoutList.findOne({
     where: {
