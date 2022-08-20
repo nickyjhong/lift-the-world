a5065f84d031211e07b93fda30167fee444eb3cc
nickyjhong <nicolehong95@gmail.com>
2022-08-19 21:55:12 -0400

    converted singleexercise to functional component

--- client/components/SingleExercise.js
+++ client/components/SingleExercise.js
@@ -1,53 +1,40 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component, useEffect } from 'react';
+import { connect, useSelector, useDispatch } from 'react-redux';
 import { fetchExercise } from '../store/singleExercise';
 import { addToWorkout } from '../store/workout';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
-class SingleExercise extends Component {
-  constructor() {
-    super();
-    this.handleAdd = this.handleAdd.bind(this);
+const SingleExercise = () => {
+  let { id } = useParams();
+
+  const exercise = useSelector((state) => state.singleExercise);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchExercise(id))
+  }, [dispatch, id]);
+
+  const handleAdd = (event) => {
+    event.preventDefault();
+    dispatch(addToWorkout(exercise))
   }
 
-  componentDidMount() {
-    this.props.getSingleExercise(this.props.match.params.id)
-  }
-
-  handleAdd() {
-    this.props.addToWorkout(this.props.exercise)
-  }
-
-  render() {
-    const { exercise } = this.props;
-    console.log('props', this.props)
-    console.log('exercise', exercise)
-    return (
-      <div>
-        {exercise && exercise.id ? (
-          <div>
-            <h3>{exercise.name}</h3>
-            <button onClick={() => this.handleAdd()}>
-              add to workout
-            </button>
-          </div>
-        ) : (
-          <div>
-            <p>No exercises found</p>
-          </div>
-        )}
-      </div>
-    )
-  }
+  return (
+    <div>
+      {exercise && exercise.id ? (
+        <div>
+          <h3>{exercise.name}</h3>
+          <button onClick={handleAdd}>
+            add to workout
+          </button>
+        </div>
+      ) : (
+        <div>
+          <p>No exercises found</p>
+        </div>
+      )}
+    </div>
+  )
 }
 
-const mapStateToProps = (state) => ({
-  exercise: state.singleExercise,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  getSingleExercise: (id) => dispatch(fetchExercise(id)),
-  addToWorkout: (exercise) => dispatch(addToWorkout(exercise)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(SingleExercise);
+export default SingleExercise
