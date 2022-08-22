40ae883866011ea66a05a2d0f63b01bf198103b5
Kyle Parkinson <kparki1130@gmail.com>
2022-08-22 17:28:57 -0400

    homepage done with CSS, needs new links eventually

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 export const Home = (props) => {
   const { username } = props;
 
   return (
-    <div>
-      <h3>Welcome, {username}</h3>
+    <div className="homepage-container">
+      <div>
+        <h3>Welcome to Lift The World, {username}</h3>
+      </div>
+      <div>
+        <Link to="/dontKnowWhatThisIsYet">
+          <button className="homepage-btn">Select Workouts</button>
+        </Link>
+      </div>
+      <div>
+        <Link to="/dontKnowWhatThisIsYet">
+          <button className="homepage-btn">Get a Pre-made Program</button>
+        </Link>
+      </div>
+      <div>
+        <Link to="/dontKnowWhatThisIsYet">
+          <button className="homepage-btn">View My Progress</button>
+        </Link>
+      </div>
+      <div>
+        <Link to="/dontKnowWhatThisIsYet">
+          <button className="homepage-btn">Chat With a Trainer</button>
+        </Link>
+      </div>
     </div>
   );
 };
