f9ac6b84df8dd24ca8baeba6ff8b396540f24a45
Cherry Xu <ms.cherry.xu@gmail.com>
2022-08-22 16:45:02 -0400

    Started working on WeightComparison component and edited seed

--- client/components/Recap.js
+++ client/components/Recap.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import WeightComparison from './WeightComparison';
 
 const Recap = () => {
   return (
@@ -6,16 +7,15 @@ const Recap = () => {
       <div className="recap-congrats">
         <p>Great job with your workout!</p>
       </div>
-      <div className="recap-weights">
-        XXXX
-      </div>
+      <div className="recap-weights">XXXX</div>
       <div className="recap-total-weight">
         <p>You lifted X pounds during this workout!</p>
         <p>That's the weight of X</p>
+        <WeightComparison />
         <img />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Recap;
