1082d37ab190c82aaa357a6ae4297b98b7eade6a
nickyjhong <nicolehong95@gmail.com>
2022-08-24 10:47:38 -0400

    able to add to workout from exercisegroup view

--- client/components/ExerciseGroup.js
+++ client/components/ExerciseGroup.js
@@ -2,8 +2,10 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from 'react-router-dom';
 import { fetchExerciseCategory } from '../store/exercises';
+import { addToWorkout } from "../store/workout";
+import { Link } from "react-router-dom";
 
-export default function ExerciseGroup() {
+export default function ExerciseGroup(props) {
   let { category } = useParams();
   const dispatch = useDispatch();
 
@@ -20,7 +22,15 @@ export default function ExerciseGroup() {
           <ul>
             {group.map((exercise) => {
               return (
-                <li key={exercise.id}>{exercise.name}</li>
+                <li 
+                  key={exercise.id}
+                  className="exercise-list-item"
+                >
+                  <Link to={`/exercise/${exercise.id}`}>
+                    {exercise.name}
+                  </Link>
+                  <button onClick={() => dispatch(addToWorkout(exercise))}>add to workout</button>
+                </li>
               )
             })}
           </ul>
