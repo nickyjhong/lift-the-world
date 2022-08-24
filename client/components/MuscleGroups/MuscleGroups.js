cc34e81f565e0dc0f2443b82a99869f7444016d8
nickyjhong <nicolehong95@gmail.com>
2022-08-24 14:13:01 -0400

    fixed overextending links

--- client/components/MuscleGroups/MuscleGroups.js
+++ client/components/MuscleGroups/MuscleGroups.js
@@ -6,34 +6,48 @@ const MuscleGroups = () => {
     <div className="musclegroup-container">
       <h1 className="musclegroup-heading">Choose a Muscle Group To Get Started!</h1>
       <div className="musclegroup-btn-container">
-        <Link to="/musclegroups/chest" className="musclegroup-btn-link">
-          <button className="musclegroup-btn">Chest</button>
-        </Link>
 
-        <Link to="/musclegroups/back" className="musclegroup-btn-link">
-          <button className="musclegroup-btn">Back</button>
-        </Link>
+        <div className="musclegroup-link-btn">
+          <Link to="/musclegroups/chest" className="musclegroup-btn-link">
+            <button className="musclegroup-btn">Chest</button>
+          </Link>
+        </div>
 
-        <Link to="/musclegroups/biceps" className="musclegroup-btn-link">
-          <button className="musclegroup-btn">Biceps</button>
-        </Link>
+        <div className="musclegroup-link-btn">
+          <Link to="/musclegroups/back" className="musclegroup-btn-link">
+            <button className="musclegroup-btn">Back</button>
+          </Link>
+        </div>
 
-        <Link to="/musclegroups/triceps" className="musclegroup-btn-link">
-          <button className="musclegroup-btn">Triceps</button>
-        </Link>
+        <div className="musclegroup-link-btn">
+          <Link to="/musclegroups/biceps" className="musclegroup-btn-link">
+            <button className="musclegroup-btn">Biceps</button>
+          </Link>
+        </div>
 
-        <Link to="/musclegroups/legs" className="musclegroup-btn-link">
-          <button className="musclegroup-btn">Legs</button>
-        </Link>
+        <div className="musclegroup-link-btn">
+          <Link to="/musclegroups/triceps" className="musclegroup-btn-link">
+            <button className="musclegroup-btn">Triceps</button>
+          </Link>
+        </div>
 
-        <Link to="/musclegroups/glutes" className="musclegroup-btn-link">
-          <button className="musclegroup-btn">Glutes</button>
-        </Link>
-        
-        <Link to="/musclegroups/core" className="musclegroup-btn-link">
-          <button className="musclegroup-btn">Core</button>
-        </Link>
+        <div className="musclegroup-link-btn">
+          <Link to="/musclegroups/legs" className="musclegroup-btn-link">
+            <button className="musclegroup-btn">Legs</button>
+          </Link>
+        </div>
 
+        <div className="musclegroup-link-btn">
+          <Link to="/musclegroups/glutes" className="musclegroup-btn-link">
+            <button className="musclegroup-btn">Glutes</button>
+          </Link>
+        </div>
+
+        <div className="musclegroup-link-btn">
+          <Link to="/musclegroups/core" className="musclegroup-btn-link">
+            <button className="musclegroup-btn">Core</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
