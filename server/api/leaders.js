7318cba4370624ca74218a983113c9a01fcbdf12
Cherry Xu <ms.cherry.xu@gmail.com>
2022-08-24 12:49:40 -0400

    Have leader board include all users (top 10)

--- server/api/leaders.js
+++ server/api/leaders.js
@@ -1,27 +1,28 @@
-const router = require('express').Router()
-const { models: { User }} = require('../db')
+const router = require("express").Router();
+const {
+  models: { User },
+} = require("../db");
 module.exports = router;
 
-router.get('/', async (req, res, next) => {
-    try {
-      const allUsers = await User.findAll();
-      const sortedUsers = allUsers.sort((a,b)=>{
-          return b.totalWeight - a.totalWeight
-      });
-      const topTen = [];
-  
-      if(sortedUsers.length >= 10){
-        console.log('wrong way')
-        for(let i = 0; i < 10; i++){
-          topTen.push(sortedUsers[i]);
-        }
-      }else{
-        for(let i = 0; i < sortedUsers.length - 1; i++){
-          topTen.push(sortedUsers[i]);
-        }
+router.get("/", async (req, res, next) => {
+  try {
+    const allUsers = await User.findAll();
+    const sortedUsers = allUsers.sort((a, b) => {
+      return b.totalWeight - a.totalWeight;
+    });
+    const topTen = [];
+
+    if (sortedUsers.length >= 10) {
+      for (let i = 0; i < 10; i++) {
+        topTen.push(sortedUsers[i]);
       }
-      res.send(topTen);
-    } catch (error) {
-      next(error)
+    } else {
+      for (let i = 0; i < sortedUsers.length; i++) {
+        topTen.push(sortedUsers[i]);
+      }
     }
-  })
+    res.send(topTen);
+  } catch (error) {
+    next(error);
+  }
+});
