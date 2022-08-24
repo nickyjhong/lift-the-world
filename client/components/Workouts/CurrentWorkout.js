431f232deb26aade2959134054f0bae6ce07d370
nickyjhong <nicolehong95@gmail.com>
2022-08-24 19:16:35 -0400

    bench press displays previous info but seed needs to be fixed

--- client/components/Workouts/CurrentWorkout.js
+++ client/components/Workouts/CurrentWorkout.js
@@ -17,54 +17,60 @@ export default function CurrentWorkout() {
       {/* <h2 className="cw-workout-name">{workout.name}</h2> */}
       {/* map each exercise below to replace hard coded stuff */}
       <div className="cw-exercise-container">
-        {workouts.map((workout) => {
+        {workouts.map((exercise) => {
           return (
             <div>
-            <h2 className="cw-exercise-name">{workout.name}</h2>
+            <h2 className="cw-exercise-name">{exercise.name}</h2>
             <div className="cw-exercise">
               <div className="cw-headings">
                 <p className="cw-heading">Set</p>
-                <p className="cw-heading">Previous</p>
+                <p className="cw-heading cw-previous-heading">Previous</p>
                 <p className="cw-heading">Reps</p>
-                <p className="cw-heading">Weight</p>
+                <p className="cw-heading cw-weight-heading">Weight</p>
                 <p className="cw-heading">️✔️️</p>
               </div>
-              <div className="cw-info-container">
-                <form>
-                  <div className="cw-set-info">
-                    <input
-                      className="cw-sr-input"
-                      type="number"
-                      name="set"
+              {exercise.workoutlist.sets.map((set, index) => {
+                return (
+                  <div className="cw-info-container">
+                    <form>
+                      <div className="cw-set-info">
+                        <input
+                          className="cw-sr-input cw-set"
+                          type="number"
+                          name="set"
+                          value={index + 1}
+                          disabled
+                        />                        
+                        
+                        <p className="cw-previous">{set.reps} x {set.weight}</p>
+                        
+                        <input
+                          className="cw-sr-input cw-rep-input"
+                          type="number"
+                          name="rep"
 
-                    />
+                        /> 
 
-                    <p className="cw-previous">rep x weight</p>
-                    
-                    <input
-                      className="cw-sr-input"
-                      type="number"
-                      name="rep"
+                        <input
+                          className="cw-weight-input"
+                          type="number"
+                          name="weight"
+        
+                        />
 
-                    /> 
+                        {/* change into a checkbox */}
+                        <input
+                          className="cw-check-input"
+                          type="text"
+                          name="check"
 
-                    <input
-                      className="cw-weight-input"
-                      type="number"
-                      name="weight"
-    
-                    />
-
-                    {/* change into a checkbox */}
-                    <input
-                      className="cw-check-input"
-                      type="text"
-                      name="check"
-
-                    />
+                        />
+                      </div>
+                    </form>
                   </div>
-                </form>
-              </div>
+                )
+              })}
+              
               <div className="cw-btn-container">
                 <button className="cw-add-btn">
                   + Add Set
