e7af1cb5f2813da7262025e1c458f2228a3caff7
Cherry Xu <ms.cherry.xu@gmail.com>
2022-08-23 23:36:44 -0400

    UserProfile page renders and logout button added

--- client/components/UserProfile.js
+++ client/components/UserProfile.js
@@ -1,22 +1,46 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { useEffect } from "react";
+import { connect, useDispatch } from "react-redux";
+import { fetchUsers } from "../store/allUsers";
+import { Link } from "react-router-dom";
+import { logout } from "../store";
 
-const UserProfile = () => {
-  const { username } = props;
-  console.log(props);
+const UserProfile = ({ handleClick, username }) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
   return (
-    <div>
-      <h1>{username}</h1>
-      <h3>Level {}</h3>
-    </div>
+    <>
+      <div className="profile-container">
+        <h1>{username}</h1>
+        <h3>Level {}</h3>
+      </div>
+      <div className="character-container">
+        <img className="character" src="/images/cat4.png" />
+        <p>Progress bar goes here</p>
+        {/* will discuss experience points with team on leveling up */}
+        <button className="homepage-btn">See my progress graph</button>
+        <Link to="/login">
+          <button className="homepage-btn" onClick={handleClick}>
+            Logout
+          </button>
+        </Link>
+      </div>
+    </>
   );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    username: state.auth.username,
-    user: state.allUsers,
-  };
-};
+const mapStateToProps = (state) => ({
+  username: state.auth.username,
+});
 
-export default connect(mapStateToProps)(UserProfile);
+const mapDispatchToProps = (dispatch) => ({
+  handleClick() {
+    dispatch(logout());
+  },
+  allUsers: () => dispatch(fetchUsers()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserProfile);
