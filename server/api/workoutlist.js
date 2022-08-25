73fb73587b7a1574ac38bc529788844dc8d8e11d
Cherry Xu <ms.cherry.xu@gmail.com>
2022-08-25 11:39:34 -0400

    Put route works on Postman

--- server/api/workoutlist.js
+++ server/api/workoutlist.js
@@ -1,77 +1,75 @@
 const router = require("express").Router();
 const {
   models: { Workout, Exercise, WorkoutList, User },
-} = require('../db/');
-const { requireToken } = require('./middleware');
+} = require("../db/");
+const { requireToken } = require("./middleware");
 module.exports = router;
 
 // ALL PREVIOUS WORKOUTLISTS - closed and active
-router.get('/', requireToken, async (req, res, next) => {
+router.get("/", requireToken, async (req, res, next) => {
   try {
     const exerciseSet = await WorkoutList.findAll({
       where: {
         userId: req.user.dataValues.id,
       },
-      order: [ [ 'createdAt', 'DESC' ]],
+      order: [["createdAt", "DESC"]],
     });
 
-    res.send(exerciseSet)
-
+    res.send(exerciseSet);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-// ALL PREVIOUS SETS FOR SPECIFIC EXERCISE 
-router.get('/:id', requireToken, async (req, res, next) => {
+// ALL PREVIOUS SETS FOR SPECIFIC EXERCISE
+router.get("/:id", requireToken, async (req, res, next) => {
   try {
     let exerciseSet = await WorkoutList.findAll({
       where: {
         exerciseId: req.params.id,
         userId: req.user.dataValues.id,
       },
-      order: [ [ 'createdAt', 'DESC' ]],
+      order: [["createdAt", "DESC"]],
     });
 
-    res.send(exerciseSet)
-
+    res.send(exerciseSet);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-// MOST RECENT SET FOR SPECIFIC EXERCISE 
-router.get('/prev/:id', requireToken, async (req, res, next) => {
+// MOST RECENT SET FOR SPECIFIC EXERCISE
+router.get("/prev/:id", requireToken, async (req, res, next) => {
   try {
     let exerciseSet = await WorkoutList.findAll({
       where: {
         exerciseId: req.params.id,
         userId: req.user.dataValues.id,
       },
-      order: [ [ 'createdAt', 'DESC' ]],
+      order: [["createdAt", "DESC"]],
     });
 
     if (exerciseSet[0].sets.length === 0) {
-      res.send(exerciseSet[1])
+      res.send(exerciseSet[1]);
     } else {
-      res.send(exerciseSet[0])
+      res.send(exerciseSet[0]);
     }
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-// UPDATE - currently not working? not sure
-router.put('/:id', requireToken, async (req, res, next) => {
+// UPDATES WORKOUT LIST SET
+router.put("/:id", requireToken, async (req, res, next) => {
   try {
     const workout = await Workout.findOne({
       where: {
         userId: req.user.dataValues.id,
-        status: 'active',
+        status: "active",
       },
       include: [Exercise],
     });
-    console.log(req.user)
+
     let exercise = await WorkoutList.findOne({
       where: {
         exerciseId: req.params.id,
@@ -79,23 +77,20 @@ router.put('/:id', requireToken, async (req, res, next) => {
       },
     });
 
-    console.log('EXERCISE', exercise)
-    console.log('DV', exercise.dataValues.sets)
-    exercise.dataValues.sets.push(req.body)
-
+    console.log("EXERCISE", exercise);
+    exercise.dataValues.sets.push(req.body);
+    exercise.save();
+    res.send(exercise);
     // res.send(
     //   await Workout.findOne({
     //     where: {
     //       userId: req.user.dataValues.id,
-    //       status: 'active',
+    //       status: "active",
     //     },
     //     include: [Exercise],
     //   })
-    // )
-
-    res.send(exercise)
-
+    // );
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
