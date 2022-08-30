2bca34930dd5688b00bb7eadb01c5fdea4dbe06d
Ryan Scoville <ryanscoville29@yahoo.com>
2022-08-30 14:57:02 -0500

    made initial sprites component

---
+++ client/components/Sprites.js
@@ -0,0 +1,18 @@
+import React, {Component, useEffect} from 'react';
+import { useSelector, useDispatch } from "react-redux";
+
+
+const ChooseSprites = ()=>{
+
+    
+    useEffect(()=>{
+
+    }, [])
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ChooseSprites;
