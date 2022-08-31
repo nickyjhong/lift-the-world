e5b96a2e368789fae841ada32a4fce602af2ae2d
Ryan Scoville <ryanscoville29@yahoo.com>
2022-08-31 11:01:36 -0500

    imported required thunk for choosing a sprite

--- client/components/Sprites.js
+++ client/components/Sprites.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import { fetchUnlockedSprites } from '../store/sprites';
+import { updateSelectedSprite } from '../store/updateSelectedSprite';
 
 
 const ChooseSprites = ()=>{
@@ -13,8 +14,8 @@ const ChooseSprites = ()=>{
     }, [dispatch]);
 
     const chooseSprite = (event)=>{
-        setSprite({name: event.name});
-        dispatch(selectSprite(currentSprite));
+        setSprite({name: event.target.name});
+        dispatch(updateSelectedSprite(currentSprite));
     }
 
     const sprites = useSelector((state)=>{
