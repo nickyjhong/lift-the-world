3387f53bb6d6cc83c6496e57b5bae59fe11a5078
nickyjhong <nicolehong95@gmail.com>
2022-09-01 18:12:44 -0400

    add to workout button added

--- client/components/Exercise/AllExercises.js
+++ client/components/Exercise/AllExercises.js
@@ -35,7 +35,7 @@ const AllExercises = () => {
       {exercises ? (
         <div className="all-exercises-container">
           <h1 className="all-exercises-heading">All Exercises</h1>
-          <ul className="all-exercises-list">
+          {/* <ul className="all-exercises-list">
             {exercises.map((exercise) => {
               return (
                 <li key={exercise.id} className="all-exercises-list-item">
@@ -57,7 +57,42 @@ const AllExercises = () => {
                 </li>
               );
             })}
-          </ul>
+          </ul> */}
+          {exercises.map((exercise) => {
+            return (
+            <div className="exercise-container" key={exercise.id}>
+              <div className="exercise-img-container">
+                <img src={exercise.image} className="exercise-img"/>
+              </div>
+              <div className="exercise-info-container">
+                <Link
+                    to={`/exercise/${exercise.id}`}
+                    className="exercise-name"
+                  >
+                    {exercise.name}
+                  </Link>
+
+                  <Link
+                    to={`/musclegroups/${exercise.category}`}
+                    className="exercise-category"
+                  >
+                    {exercise.category}
+                  </Link>
+              </div>
+              <div className="exercise-btn-container">
+                <button
+                  onClick={() => {
+                    dispatch(addToWorkout(exercise));
+                    // notification("info");
+                  }}
+                  className="exercise-add-btn"
+                >
+                  <img src="/images/dumbbell.png" className="exercise-add-btn-img" />
+                </button>
+              </div>
+            </div>
+            )
+          })}
         </div>
       ) : (
         <div>
