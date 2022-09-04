4726acb63207478003f2ae0699df5b2fa6a606d0
Cherry Xu <41312534+mscherryxu@users.noreply.github.com>
2022-09-04 12:53:52 -0400

    Changed color of nav icons and deleted unused images (#174)

--- client/store/userStats.js
+++ client/store/userStats.js
@@ -1,36 +1,37 @@
-import axios from 'axios';
+import axios from "axios";
 
-const USER_STATS = 'USER_STATS';
+const USER_STATS = "USER_STATS";
 
-const userStats = (stats)=>{
-    return {
-        type: USER_STATS,
-        stats
-    }
+const userStats = (stats) => {
+  return {
+    type: USER_STATS,
+    stats,
+  };
 };
 
-export const fetchUserStats = ()=>{
-    return async (dispatch)=>{
-        try {
-            const token = window.localStorage.getItem("token");
-            const {data: stats} = await axios.get('/api/users/stats', {headers:{
-                authorization: token
-            }});
-            dispatch(userStats(stats));
-        } catch (error) {
-            console.log(error)
-        }
-    
+export const fetchUserStats = () => {
+  return async (dispatch) => {
+    try {
+      const token = window.localStorage.getItem("token");
+      const { data: stats } = await axios.get("/api/users/stats", {
+        headers: {
+          authorization: token,
+        },
+      });
+      dispatch(userStats(stats));
+    } catch (error) {
+      console.log(error);
     }
-}
+  };
+};
 
-const inittialState = {};
+const initialState = {};
 
-export default function userStatsReducer(state = inittialState, action){
-    switch(action.type){
-        case USER_STATS:
-            return action.stats;
-        default:
-            return state;
-    }
+export default function userStatsReducer(state = initialState, action) {
+  switch (action.type) {
+    case USER_STATS:
+      return action.stats;
+    default:
+      return state;
+  }
 }
